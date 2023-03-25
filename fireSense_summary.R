a89634d7b62a5b78526c39e35a17d18a58ce6ac0
Alex Chubaty <achubaty@for-cast.ca>
2023-03-25 12:39:44 -0600

    add disk.frame to reqdPkgs
    one of the LandR Bimoass modules uses it - cannot load simlist objs w/o it

--- fireSense_summary.R
+++ fireSense_summary.R
@@ -14,7 +14,7 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.md", "fireSense_summary.Rmd"), ## same file
-  reqdPkgs = list("assertthat", "cowplot", "data.table", "fs",
+  reqdPkgs = list("assertthat", "cowplot", "data.table", "disk.frame", "fs",
                   "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9048)",
                   "ggplot2", "googledrive", "purrr", "raster", "rasterVis", "RColorBrewer",
                   "SpaDES.core (>= 1.0.10)", "SpaDES.tools", "qs"),
